320a1d87e0e554d7ff2af4452bc47a7597504c25
xu bg <xlsfs@users.noreply.github.com>
2023-03-31 19:06:24 +0800

    Update main.js
    解决导出的“B”字符在threejs下下面的洞被忽略的问题

--- javascripts/main.js
+++ javascripts/main.js
@@ -216,3 +216,66 @@ var reverseCommands = function(commands){
     return reversed;
     
 };
+
+var reverseCommands = function(commands){
+    
+    var paths = [];
+    var path;
+    
+    commands.forEach(function(c){
+        if (c.type.toLowerCase() === "m"){
+            path = [c];
+            paths.push(path);
+        } else if (c.type.toLowerCase() !== "z") {
+            path.push(c);
+        }
+    });
+    
+    var reversed = [];
+    paths.forEach(function(p){
+        var result = {"type":"m" , "x" : p[p.length-1].x, "y": p[p.length-1].y};
+        reversed.push(result);
+        
+        for(var i = p.length - 1;i > 0; i-- ){
+            var command = p[i];
+            result = {"type":command.type};
+            if (command.x2 !== undefined && command.y2 !== undefined){
+                result.x1 = command.x2;
+                result.y1 = command.y2;
+                result.x2 = command.x1;
+                result.y2 = command.y1;
+            } else if (command.x1 !== undefined && command.y1 !== undefined){
+                result.x1 = command.x1;
+                result.y1 = command.y1;
+            }
+            result.x =  p[i-1].x;
+            result.y =  p[i-1].y;
+            reversed.push(result);
+        }
+        
+    });
+    
+    return reversed;
+    
+};
+
+function area( contour ) {
+
+    const n = contour.length;
+    let a = 0.0;
+
+    for ( let p = n - 1, q = 0; q < n; p = q ++ ) {
+
+        a += contour[ p ].x * contour[ q ].y - contour[ q ].x * contour[ p ].y;
+
+    }
+
+    return a * 0.5;
+
+}
+
+function isClockWise( pts ) {
+
+    return area( pts ) < 0;
+
+}
